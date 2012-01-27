459ce002ade65daf5b0cde64d78ec31d3e00e9ac
unknown <DaftAlex@DaftAlex7.(none)>
2012-01-27 00:13:49 -0600

    1-26-12
    We tried finding the solution to the login page.
    We now have login page that will take the data from the database and render it into our view files with username and id passed from the controller file.
    It also checks the validation of username and password and redirects it accordingly.
    We also modified the controller page of site/index to redirect to our login page.
    
    The updated file should be available in our github repo.
    
    Next meeting will be on Tuesday 31st Jan after class.
    
    Thanks,
    -Yojan
       - and Alex!

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -29,6 +29,21 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
+		
+		//render homepage as our Pagoda Login instead of demo mainpage 
+		$this->render('/login/index');
+	}
+	
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionHome()
+	{
+		// renders the view file 'protected/views/site/index.php'
+		// using the default layout 'protected/views/layouts/main.php'
+		
+		//render homepage as our Pagoda Login instead of demo mainpage 
 		$this->render('index');
 	}
 
